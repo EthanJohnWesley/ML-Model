aa122417c2fa72fbdefd05c41a045b6050a65655
Ethan Vijayabaskaran <ethan.vijayabaskaran@chevron.net>
2024-12-18 16:45:49 -0600

    added files

---
+++ ml-project/target/debug/deps/ml_project-fcb39ec575a9524d.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/zque/rust/ml-project/ml-project/target/debug/deps/ml_project-fcb39ec575a9524d: src/main.rs
+
+/mnt/c/Users/zque/rust/ml-project/ml-project/target/debug/deps/ml_project-fcb39ec575a9524d.d: src/main.rs
+
+src/main.rs:
---
+++ ml-project/target/debug/ml-project.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/zque/rust/ml-project/ml-project/target/debug/ml-project: /mnt/c/Users/zque/rust/ml-project/ml-project/src/main.rs
